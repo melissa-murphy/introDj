fa5d5bc925d20b5dd502baf6385df4f08caad608
melissa-murphy <melissamurphy.ca@gmail.com>
2020-02-10 15:45:00 -0800

    migrations working, admin good

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Notes
+from .models import NoteAdmin
+from .models import PersonalNote
 
 # Register your models here.
+admin.site.register(Notes, NoteAdmin, )
+admin.site.register(PersonalNote, )
--- notes/models.py
+++ notes/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 
@@ -7,8 +9,19 @@ class Notes(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
 
-class NotesCategories(models.Model):
+class NoteUser(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, default="Smith")
+    note_type = models.CharField(max_length=50, blank=True)
+
+
+class NoteAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'last_modified')
+
+class PersonalNote(Notes):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
